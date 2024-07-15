723b4ec5048d33856bd02cc18e2fd20a2de8bf81
NeilDogii <aryaworrior41@gmail.com>
2024-07-15 20:57:49 +0530

    inital panda

---
+++ main.py
@@ -0,0 +1,52 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Message
+from discord.ext import commands
+import customCommands
+
+
+#setup
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+intents: Intents = Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+
+#add commands
+for command in customCommands.commandList:
+    bot.add_command(command)
+
+
+#events
+@bot.event
+async def on_ready() -> None:
+    print(f'{bot.user} has connected to Discord!')
+
+
+@bot.event
+async def on_message(message: Message) -> None:
+    if message.author == bot.user:
+        return
+
+    username: str = message.author.name
+    user_message: str = message.content
+    channel: str = message.channel.name
+
+    print(f"User: {username} | Message: {user_message} | Channel: {channel}")
+
+    
+    await bot.process_commands(message)
+
+
+
+#main
+def main() -> None:
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
+
